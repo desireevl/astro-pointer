ebd255a7c571b50cd403c77387d54304b78eac33
desiree <desireevogtlee@gmail.com>
2017-08-22 00:36:22 +1000

    added satellites

--- d/obj_loc.py
+++ d/obj_loc.py
@@ -1,14 +1,16 @@
 from skyfield.api import Topos, load
 
-# get coordinates
-# get location of cel objs
+# get current coordinates from phone
+# hamming distance for searching
+# searchable constellations/stars
+# browse celestrak for these
 
 # get latitude and longitude from phone location
-latitude = '42.3583 N'
-longitude = '71.0603 W'
+latitude = '-27.469771 S'
+longitude = '153.025124 E'
 
-#get desired object
-des_obj = 'Mars'
+# get desired object
+des_obj = 'ISS (ZARYA)'
 
 # gets current time
 ts = load.timescale()
@@ -17,21 +19,34 @@ curr_time = ts.now()
 # loads ephemeris, gets planet locations
 planets = load('de421.bsp')
 
-# obj with capital letter
+# gets satellite element set
+# celestrak updates positions
+stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
+satellites = load.tle(stations_url)
+
 def obj_location(obj, lat, long):
-    curr_obj = planets[obj]
-    earth = planets['Earth']
+    try:
+        curr_obj = planets[obj]
+        earth = planets['Earth']
 
-    # topocentric is position measured relative to location on earth
-    curr_loc = earth + Topos(lat, long)
-    # .observe backdates to account for c (astrometric), .apparent takes deflection and abberation into account
-    apparent_loc = curr_loc.at(curr_time).observe(curr_obj).apparent()
+        # topocentric is position measured relative to location on earth
+        curr_loc = earth + Topos(lat, long)
+        # .observe backdates to account for c (astrometric), .apparent takes deflection and abberation into account
+        apparent_loc = curr_loc.at(curr_time).observe(curr_obj).apparent()
 
-    # positive altitude is above horizon, negative is below
-    # azimuth is angle, 0deg at north
-    # altaz(temperature_C= , pressure_mbar= )
-    alt, az, distance = apparent_loc.altaz()
+        # positive altitude is above horizon, negative is below
+        # azimuth is angle, 0deg at north
+        # altaz(temperature_C= , pressure_mbar= )
+        alt, az, distance = apparent_loc.altaz()
 
-    return(alt.degrees, az.degrees, distance)
+    except:
+        satellite = satellites[obj]
+        # subtracts vector location of observer from satellite
+        difference = satellite - Topos(lat,long)
+        # converts this difference to coordinates
+        topocentric = difference.at(curr_time)
+        alt, az, distance = topocentric.altaz()
+
+    return alt.degrees, az.degrees, distance
 
 print(obj_location(des_obj, latitude, longitude))
